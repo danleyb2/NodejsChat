0780c377245d4f3fbc555a450312fb8367e453ec
danleyb2 <ndieksman@gmail.com>
2015-07-06 17:22:30 +0300

    update

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ app.use('/chat', function (req,res,next) {
     var io=require('socket.io')(require('./includes/session').serverObj);
     res.sendFile(__dirname+'/index.html');
     io.on('connection',function(socket){
-       console.log("A user connected");
+
         //setTimeout(function(){io.emit('chat message',"Welcome");},5000);
         socket.on('chat message',function(msg){
-
-            console.log(JSON.parse(msg));
-
-           io.emit('chat message',msg);
+            io.emit('chat message', msg);
         });
     });
     /*
@@ -32,10 +29,6 @@ app.get('/lib/jquery.js',function(req,res){
 app.get('/socket.io/socket.io.js',function(req,res){
     res.sendFile(__dirname+'/public/scripts/socket.io.js');
 });
-app.use(function(req, res, next) {
-    console.log("handling a request: "+req.url);
-    res.end();
-    });
 
 
 
