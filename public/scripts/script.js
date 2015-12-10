da37e004f3e5e9121d54a0eb913668e24fc93fdc
danleyb2 <ndieksman@gmail.com>
2015-12-10 17:57:55 +0300

    update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,14 +1,14 @@
-var username = null,
-    socket=io();
+var username = null;
+var socket = io();
 
-
+socket.on('message', function (df) {
+    console.log(df.sock);
+});
 socket.on('chat message',function(mesg){
-    var msg=JSON.parse(mesg);
-
+    console.log(mesg);
+    var msg = mesg;
 
     var div=document.createElement('DIV');
-
-
     if(msg.sender==username){
         div.innerHTML="You-:"+msg.contet;
         div.setAttribute('class','b');
@@ -16,22 +16,20 @@ socket.on('chat message',function(mesg){
         div.innerHTML=msg.sender+"-:"+msg.contet;
         div.setAttribute('class','a');
     }
-
-    $('#history').append(div);
+    $("#history").append(div);
 });
 
 function MessageObj(sender, content) {
     this.sender=sender;
     this.contet=content;
 }
-
 function addNew(){
     username=document.getElementById('username').value.toString().trim();
     if(username!=="") {
         //add user
 
         document.getElementById('username').value="";
-        $("#hideE").fadeOut(4000);
+        $("#hideE").fadeOut(3000);
 
     }else{
         alert("Username cannot be empty");
@@ -49,7 +47,8 @@ function sendNew(){
     if(msg!=="") {
         //send message
         var ms = new MessageObj(username, msg);
-        socket.emit('chat message',JSON.stringify(ms));
+        socket.emit('chat message', /*JSON.stringify(*/ms/*)*/);
+
         document.getElementById('txt').value = "";
 
         return false;
